8166c196013ee19f2e8a425a6ecb2455fafa57f0
miyamoto <shahrahul1618@gmail.com>
2023-09-11 01:01:43 +0530

    feedback schema

--- models/feedback.model.js
+++ models/feedback.model.js
@@ -1,27 +1,23 @@
 import mongoose from "mongoose"
 
-const lawyerSchema = new mongoose.Schema({
-    FIRSTNAME: {
+const feedbackSchema = new mongoose.Schema({
+    LAWYER_ID: {
         type: String,
     },
-    LASTNAME: {
+    USER_ID: {
         type: String,
-    },
-    CITY: {
-        type: String, 
-    },
-    STATE: {
-        type: String, 
     },
     TIME: {
         type: String, 
     },
     FEEDBACK: {
         type: String,
+    },
+    RATING: {
+        type: Number,
     }
 });
 
-const Lawyer = mongoose.model('Lawyer', lawyerSchema);
-
-export default Lawyer;
+const feedback = mongoose.model("feedback", feedbackSchema);
+export default feedback;
 
