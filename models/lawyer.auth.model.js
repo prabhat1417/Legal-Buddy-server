7ba4266c16e9611aee1660abea2b14f57d54cf0b
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-09-10 22:07:55 +0530

    added lawyer data model

--- models/lawyer.auth.model.js
+++ models/lawyer.auth.model.js
@@ -1,54 +1,7 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose"
 
-const lawyerSchema = new mongoose.Schema({
-    FIRSTNAME: {
-        type: String,
-        required: true
-    },
-    LASTNAME: {
-        type: String,
-        required: true
-    },
-    EMAIL: {
-        type: String,
-        required: true
-    },
-    MOBILENUMBER: {
-        type: String,
-        required: true
-    },
-    PASSWORD: {
-        type: String,
-        required: true
-    },
-    GENDER: {
-        type: String,
-        required: true
-    },
-    STATE: {
-        type: String,
-        required: true
-    },
-    CITY: {
-        type: String,
-        required: true
-    },
-    BAR_COUNCIL_ID: {
-        type: String,
-        required: true
-    },
-    ID_NUMBER: {
-        type: String,
-        required: true
-    },
-    YEAR: {
-        type: String,
-        required: true
-    },
-    BAR_CERTIFICATE: {
-        type: String,
-        required: true
-    }
+const lawyerDataSchema = new mongoose.Schema({
+    
 });
 
-module.exports = mongoose.model('Lawyer', lawyerSchema);
+module.exports = mongoose.model('LawyerData', lawyerDataSchema);
