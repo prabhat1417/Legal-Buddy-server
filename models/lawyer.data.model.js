07a7c83a93e2bfb6ce575d26b71a74818ac54405
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-09-10 23:01:14 +0530

    added layer data fields

--- models/lawyer.data.model.js
+++ models/lawyer.data.model.js
@@ -1,7 +1,42 @@
 import mongoose from "mongoose"
 
 const lawyerDataSchema = new mongoose.Schema({
-    
+    FIRSTNAME: {
+        type: String,
+        required: true
+    },
+    LASTNAME: {
+        type: String,
+        required: true
+    },
+    SERVICE_CHARGE: {
+        type: String
+    },
+    BIO: {
+        type: String
+    },
+    EXPERIENCE: {
+        type: String
+    },
+    LANGUAGES: {
+        type: [String]
+    },
+    LOCATION: {
+        type: String
+    },
+    SPECIALITIES: {
+        type: [String]
+    },
+    BOOKED_BY: {
+        type: [String]
+    },
+    CASES_ASSIGNED: {
+        type: Number
+    },
+    CASES_SOLVED: {
+        type: Number
+    }
+
 });
 
 const lawyerData = mongoose.model('LawyerData', lawyerDataSchema);
